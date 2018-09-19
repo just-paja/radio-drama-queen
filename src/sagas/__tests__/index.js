b603e6e5733c836799db3665aeffb1a4945430bf
Pavel Žák <pavel@berrycloud.cz>
2018-09-19 19:53:35 +0200

    Add main saga smoke test

---
+++ src/sagas/__tests__/index.js
@@ -0,0 +1,22 @@
+import getSagaTester from '../../../mock/sagaTester';
+
+import * as logger from '../../clientLogger';
+import mainSaga from '..';
+
+describe('main project saga', () => {
+  beforeEach(() => {
+    jest.spyOn(logger, 'logWarning').mockImplementation();
+    jest.spyOn(logger, 'logError').mockImplementation();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('starts without errors', () => {
+    const sagaTester = getSagaTester();
+    expect(() => {
+      sagaTester.run(mainSaga);
+    }).not.toThrow();
+  });
+});
