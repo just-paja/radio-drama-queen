b74d1fac6747ba3f8c69b077c3d7a35dee2fa7de
just-paja <just.paja@gmail.com>
2018-09-23 02:06:58 +0200

    Playing with layout, library

--- src/soundGallery/components/SoundGalleryItem.jsx
+++ src/soundGallery/components/SoundGalleryItem.jsx
@@ -1,20 +1,46 @@
 import React from 'react';
 
+import { AddCircle, PlayCircleFilled } from '@material-ui/icons';
+import { withStyles } from '@material-ui/core/styles';
+
 import SoundTag from '../../tags/containers/SoundTag';
 
 import { GallerySound } from '../proptypes';
+import { Classes } from '../../proptypes';
 
-const SoundGalleryItem = ({ sound }) => (
-  <div>
-    {sound.name}
-    {sound.tags.map(tag => (
-      <SoundTag tag={tag} key={tag.name} />
-    ))}
+const styles = theme => ({
+  controls: {
+    marginRight: theme.spacing.unit,
+  },
+  item: {
+    display: 'flex',
+    alignItems: 'top',
+  },
+  tags: {
+    color: theme.palette.text.hint,
+  },
+});
+
+const SoundGalleryItem = ({ classes, sound }) => (
+  <div className={classes.item}>
+    <div className={classes.controls}>
+      <AddCircle />
+      <PlayCircleFilled />
+    </div>
+    <div>
+      <span>{sound.name}</span>
+      <div className={classes.tags}>
+        {sound.tags.map(tag => (
+          <SoundTag tag={tag} key={tag.name} />
+        ))}
+      </div>
+    </div>
   </div>
 );
 
 SoundGalleryItem.propTypes = {
+  classes: Classes.isRequired,
   sound: GallerySound.isRequired,
 };
 
-export default SoundGalleryItem;
+export default withStyles(styles)(SoundGalleryItem);
