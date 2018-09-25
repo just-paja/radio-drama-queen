bb3e0e2ebac937fcfec4541c689a185d7bf7f5ee
Pavel Žák <pavel@berrycloud.cz>
2018-09-25 17:27:37 +0200

    Ignore diacritics in search

--- src/search.js
+++ src/search.js
@@ -1,14 +1,15 @@
-export const clearSearch = search => search
+import { remove } from 'diacritics';
+
+export const clearSearch = search => remove(search)
   .trim()
   .replace(/[\s]/g, '-')
   .replace(/[-]+/g, '-');
 
 export const stringSearch = (sample, search) => {
   const cleanSearch = clearSearch(search)
-
     .toLowerCase()
     .split('-');
-  const cleanSample = sample.toLowerCase();
+  const cleanSample = remove(sample.toLowerCase());
   const results = cleanSearch.map(pattern => cleanSample.search(pattern));
   const searchResult = {
     relevant: results.every(item => item !== -1),
