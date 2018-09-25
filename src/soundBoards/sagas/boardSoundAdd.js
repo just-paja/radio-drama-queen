53e7abc14edbefb7e4947d04c02fd72c7d3d326f
just-paja <just.paja@gmail.com>
2018-09-25 02:50:38 +0200

    Load sound on board add

--- src/soundBoards/sagas/boardSoundAdd.js
+++ src/soundBoards/sagas/boardSoundAdd.js
@@ -1,12 +1,14 @@
 import { call, put, takeEvery } from 'redux-saga/effects';
 
 import { soundBoard } from '../actions';
+import { soundList } from '../../sounds/actions';
 import { categoryList } from '../../soundCategories/actions';
 import { createDefaultCategory } from './boardCategoryCreateDefault';
 
 function* addSound({ payload, meta: { uuid } }) {
   const category = yield call(createDefaultCategory, uuid);
   yield put(categoryList.soundAdd(category.uuid, payload));
+  yield put(soundList.loadTrigger(payload));
 }
 
 function* handleSoundAdd() {
