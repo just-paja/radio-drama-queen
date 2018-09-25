6a5d04d2abe5b80d24aa6a5a8adeacdbff4defcb
Pavel Žák <pavel@berrycloud.cz>
2018-09-25 22:27:13 +0200

    Extract onchange method

--- src/soundCategories/components/VolumeControl.jsx
+++ src/soundCategories/components/VolumeControl.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 import Slider from '@material-ui/lab/Slider';
 
 import { withStyles } from '@material-ui/core/styles';
@@ -13,24 +13,37 @@ const styles = {
   },
 };
 
-const VolumeControl = ({
-  classes,
-  muted,
-  onChange,
-  onMuteToggle,
-  volume,
-}) => (
-  <div className={classes.inline}>
-    <VolumeToggleButton muted={muted} onClick={onMuteToggle} />
-    <Slider
-      max={100}
-      min={0}
-      onChange={onChange}
-      value={muted ? 0 : volume}
-    />
-  </div>
+class VolumeControl extends Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-);
+  handleChange(event, value) {
+    const { onChange } = this.props;
+    onChange(value);
+  }
+
+  render() {
+    const {
+      classes,
+      muted,
+      onMuteToggle,
+      volume,
+    } = this.props;
+    return (
+      <div className={classes.inline}>
+        <VolumeToggleButton muted={muted} onClick={onMuteToggle} />
+        <Slider
+          max={100}
+          min={0}
+          onChange={this.handleChange}
+          value={muted ? 0 : volume}
+        />
+      </div>
+    );
+  }
+}
 
 VolumeControl.propTypes = {
   classes: PropTypes.objectOf(PropTypes.string).isRequired,
