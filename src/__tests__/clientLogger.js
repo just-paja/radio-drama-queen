7a3bc7d367dc663a39469ffa357ef255e1d637a9
just-paja <just.paja@gmail.com>
2018-09-29 22:29:00 +0200

    Cover client logger just for fun

---
+++ src/__tests__/clientLogger.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+
+import { logError, logWarning } from '../clientLogger';
+
+describe('Client logger', () => {
+  beforeEach(() => {
+    jest.spyOn(console, 'error').mockImplementation();
+    jest.spyOn(console, 'warn').mockImplementation();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('logError logs error to the console', () => {
+    const error = new Error('foo');
+    logError(error);
+    expect(console.error).toHaveBeenCalledTimes(1);
+    expect(console.error).toHaveBeenCalledWith(error);
+  });
+
+  it('logWarning logs error to the console', () => {
+    const error = new Error('foo');
+    logWarning(error);
+    expect(console.warn).toHaveBeenCalledTimes(1);
+    expect(console.warn).toHaveBeenCalledWith(error);
+  });
+});
