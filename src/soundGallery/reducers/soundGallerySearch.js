7a769ac31850f46cb1dc65fa074c67738dfd736e
just-paja <just.paja@gmail.com>
2018-09-30 01:53:04 +0200

    Refine sound gallery search reducer

--- src/soundGallery/reducers/soundGallerySearch.js
+++ src/soundGallery/reducers/soundGallerySearch.js
@@ -11,10 +11,7 @@ const initialState = {
 const soundGallerySearch = handleActions({
   [gallerySearch.CHANGE]: changeParam('search', 'payload'),
   [gallerySearch.CLEAR]: replaceState(initialState),
-  [gallerySearch.FILTER_USED_CHANGE]: (state, action) => ({
-    ...state,
-    filterUsed: action.payload,
-  }),
+  [gallerySearch.FILTER_USED_CHANGE]: changeParam('filterUsed', 'payload'),
 }, initialState);
 
 export default soundGallerySearch;
