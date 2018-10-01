3d575b726d9d50479990efa51b5cb78e9eddccfe
Pavel Žák <pavel@berrycloud.cz>
2018-10-01 09:23:58 +0200

    Cover SoundBoard add sound saga

---
+++ src/soundBoards/sagas/__tests__/boardSoundAdd.js
@@ -0,0 +1,96 @@
+import sagas from '..';
+import getSagaTester from '../../../../mock/sagaTester';
+
+import { soundBoard } from '../../actions';
+import { soundList } from '../../../sounds/actions';
+
+describe('boardSoundAdd saga', () => {
+  it('creates default board category with the sound given it does not exist', () => {
+    const sagaTester = getSagaTester({
+      soundBoards: {
+        list: [
+          {
+            uuid: 'board-1',
+          },
+        ],
+      },
+      sounds: {
+        list: [
+          {
+            uuid: 'sound-1',
+            path: 'http://example.com/sound.mp3',
+          }
+        ],
+      },
+    });
+    sagaTester.runAll(sagas);
+    sagaTester.dispatch(soundBoard.soundAdd('board-1', 'sound-1'));
+    expect(sagaTester.getState().soundCategories.list).toContainEqual(
+      expect.objectContaining({
+        name: null,
+        board: 'board-1',
+        sounds: ['sound-1'],
+      })
+    );
+  });
+
+  it('adds sound to the default board category given it exists', () => {
+    const sagaTester = getSagaTester({
+      soundBoards: {
+        list: [
+          {
+            uuid: 'board-1',
+          },
+        ],
+      },
+      soundCategories: {
+        list: [
+          {
+            uuid: 'category-1',
+            board: 'board-1',
+            sounds: [],
+          }
+        ],
+      },
+      sounds: {
+        list: [
+          {
+            uuid: 'sound-1',
+            path: 'http://example.com/sound.mp3',
+          }
+        ],
+      },
+    });
+    sagaTester.runAll(sagas);
+    sagaTester.dispatch(soundBoard.soundAdd('board-1', 'sound-1'));
+    expect(sagaTester.getState().soundCategories.list).toContainEqual(
+      expect.objectContaining({
+        uuid: 'category-1',
+        board: 'board-1',
+        sounds: ['sound-1'],
+      })
+    );
+  });
+
+  it('triggers sound load', () => {
+    const sagaTester = getSagaTester({
+      soundBoards: {
+        list: [
+          {
+            uuid: 'board-1',
+          },
+        ],
+      },
+      sounds: {
+        list: [
+          {
+            uuid: 'sound-1',
+          },
+        ],
+      },
+    });
+    sagaTester.runAll(sagas);
+    sagaTester.dispatch(soundBoard.soundAdd('board-1', 'sound-1'));
+    expect(sagaTester.getCalledActions()).toContainEqual(soundList.loadTrigger('sound-1'));
+  });
+});
