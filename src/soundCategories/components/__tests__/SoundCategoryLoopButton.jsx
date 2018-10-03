f7818e9eea02543b11e6238d1d84055ba82b91cd
Pavel Žák <pavel@berrycloud.cz>
2018-10-03 19:17:20 +0200

    Cover Category Loop button

---
+++ src/soundCategories/components/__tests__/SoundCategoryLoopButton.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { shallow } from 'enzyme';
+
+import { SoundCategoryLoopButton } from '..';
+
+describe('SoundCategoryLoopButton component', () => {
+  it('renders in primary color given it is on', () => {
+    const comp = shallow(
+      <SoundCategoryLoopButton
+        onClick={() => {}}
+        loop
+      />
+    );
+    expect(comp.find('SoundCategoryIconButton')).toHaveProp('color', 'primary');
+  });
+
+  it('renders in default color given it is off', () => {
+    const comp = shallow(
+      <SoundCategoryLoopButton
+        onClick={() => {}}
+        loop={false}
+      />
+    );
+    expect(comp.find('SoundCategoryIconButton')).toHaveProp('color', 'default');
+  });
+});
