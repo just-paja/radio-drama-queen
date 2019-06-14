cfb075f103291924edfbbc97461164700c044a06
Pavel Žák <pavel@berrycloud.cz>
2019-06-14 23:58:48 +0200

    Remove forgotten console.log

--- src/electron/handlers/soundRegister.js
+++ src/electron/handlers/soundRegister.js
@@ -3,7 +3,6 @@ module.exports = soundManager => (messenger, routine, action) =>
     .then(sound => soundManager.describeSound(sound)
       .then(sound => messenger.sendMessage(routine.success(sound.uuid, sound)))
       .catch(error => {
-        console.error(error)
         messenger.sendMessage(routine.failure(sound.uuid, error.message))
       })
     )
